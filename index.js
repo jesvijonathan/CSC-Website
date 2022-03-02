f95cba383619d4a7feec498075132ce65003be2e
jesvijonathan <jesvi22j@gmail.com>
2022-03-02 22:10:16 +0530

    splash, hamburger menu update

--- index.js
+++
@@ -1,8 +0,0 @@
-// getting the target element
-
-// example for addding some-class to the element
-
-// multiple classes can be added like this
-// myElement.classList.add("one-class", "one-more-class");
-
-// example for removing any class from the element
